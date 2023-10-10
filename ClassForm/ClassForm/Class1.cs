892fa8349ea7d1ab3cae079d45b7bc1c23176d3e
dima20004 <117287272+dima20004@users.noreply.github.com>
2023-10-11 01:08:35 +0300

    Add files via upload

---
+++ ClassForm/ClassForm/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMassInt
+{
+    public class Сmass
+    {
+        private int[] massIn; //Массив целых чисел
+
+        public int getLengthMassIn //Свойство для определения длины массива
+        {
+            get { return massIn.Length; }
+        }
+
+        public int this[int i]   //Индексатор для доступа к элементам поля-массива, 
+        {
+            get { return massIn[i]; }
+            set { massIn[i] = value; }
+        }
+
+        public Сmass(int length) //Конструктор с одним параметром –  длина массива
+        {
+
+            massIn = new int[length];
+        }
+
+        public Сmass(int[] massIntChis) //Конструктор с одним параметром – массив целых чисел
+        {
+            massIn = new int[massIntChis.Length];
+            massIn = massIntChis;
+        }
+
+        //перегруженные методы для поиска максимального элемента во всем массиве и
+        //для поиска максимального элемента в части массива,
+        //ограниченной начальным и конечным значениями индекса, передаваемых в метод в качестве параметров
+
+        public int searchMax() //Метод для поиска максимального элемента во всем массиве
+        {
+            int max = massIn[0];
+
+            for (int i = 1; i < massIn.Length; i++)
+            {
+                if (max < massIn[i])
+                {
+                    max = massIn[i];
+                }
+            }
+            return max;
+        }
+
+        public int searchMax(int nach, int conch) //Метод для поиска максимального элемента, ограниченный
+        {                                         //начальным и конечный значениями индекса
+            int max = massIn[nach];
+
+            for (int i = nach + 1; i <= conch; i++)
+            {
+                if (max < massIn[i])
+                {
+                    max = massIn[i];
+                }
+            }
+            return max;
+        }
+
+
+    }
+    public class Class1
+    {
+    }
+}
---
+++ ClassForm/ClassForm/Form1.cs
@@ -0,0 +1,132 @@
+﻿using ClassMassInt;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int length = Convert.ToInt32(textBox1.Text);
+
+                Сmass a = new Сmass(length);
+
+                //свойство определяет длину массива
+                string str = Convert.ToString(a.getLengthMassIn);
+                //индексатор позволяет получить доступ к элементу 
+                string str1 = "0";
+
+                //Метод вычисляет максимальный элемент во всем массиве
+                string str2 = Convert.ToString(a.searchMax());
+
+                //Метод вычисляет максимальный элемент во всем массиве
+                string str3 = "0";
+
+                MessageBox.Show("Свойство длина массива: " + str + "" + "\nИндексатор: " + str1 + "\nМетод вычисляет макс. элемент в массиве: " + str2 + "\nМетод вычисляет макс. элемент в интервале: 0");
+            }
+            catch
+            {
+                MessageBox.Show("Увы, у вас ошибка! Программист ленивый, поэтому сами ищите ее!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int length = dataGridView2.ColumnCount = Convert.ToInt32(textBox2.Text);
+                dataGridView2.RowCount = 1; //Количество строк
+            }
+            catch
+            {
+                MessageBox.Show("Увы, у вас ошибка! Программист ленивый, поэтому сами ищите ее!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+                try
+                {
+                    int length = Convert.ToInt32(textBox2.Text);
+
+            int[] mass = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                mass[i] = Convert.ToInt32(dataGridView2[i, 0].Value);
+            }
+
+            Сmass b = new Сmass(mass);
+
+            string str3 = textBox3.Text;
+            string str4 = textBox4.Text;
+            string str5 = textBox5.Text;
+          
+            int index = 0;
+            string str1 = "";
+
+            if (str3 != "" && str4 != "")
+            {
+
+
+                int nach = Convert.ToInt32(textBox3.Text);
+                int conch = Convert.ToInt32(textBox4.Text);
+                str3 = Convert.ToString(b.searchMax(nach, conch));
+
+
+            }
+            else
+            {
+                str3 = "";
+                str4 = "";
+            }
+
+           if (str5 != "")
+            {
+                index = Convert.ToInt32(textBox5.Text);
+                str1 = Convert.ToString(b[index]);
+            }
+            //свойство определяет длину массива
+            string str = Convert.ToString(b.getLengthMassIn);
+            //индексатор позволяет получить доступ к элементу 
+            
+
+            //Метод вычисляет максимальный элемент во всем массиве
+            string str2 = Convert.ToString(b.searchMax());
+
+            //Метод вычисляет максимальный элемент во всем массиве
+           
+
+            MessageBox.Show("Свойство длина массива: " + str + "" + "\nИндексатор: " + str1 + "\nМетод вычисляет макс. элемент в массиве: " + str2 + "\nМетод вычисляет макс. элемент в интервале: "+str3);
+            }
+            catch
+            {
+                MessageBox.Show("Увы, у вас ошибка! Программист ленивый, поэтому сами ищите ее!");
+            }
+        }
+
+       
+
+
+    }
+}
